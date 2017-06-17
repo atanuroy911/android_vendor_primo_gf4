452c88c71b5e382663819208d0b7e056fe97ebd9
atanuroy911 <atanuroy911@gmail.com>
2017-06-17 13:00:47 +0600

    Add files via upload

---
+++ primo_gf4-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/primo_GF4/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/primo_GF4/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/primo_GF4/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/primo_GF4/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/primo_GF4/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/primo_GF4/vendor,system/vendor)
